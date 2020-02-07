e099fd09fa357d8a8675b576cc8772e7410b68eb
Yesthink <kingcarnam@gmail.com>
2020-02-07 10:01:20 +0900

    Make domain

--- src/main/java/com/yesthink/website/WebsiteApplication.java
+++ src/main/java/com/yesthink/website/WebsiteApplication.java
@@ -2,9 +2,11 @@ package com.yesthink.website;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import com.yesthink.website.Common.CustomBanner;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class WebsiteApplication {
 
---
+++ src/main/java/com/yesthink/website/domain/Account.java
@@ -0,0 +1,20 @@
+package com.yesthink.website.domain;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Table
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Account extends BaseEntity {
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+}
